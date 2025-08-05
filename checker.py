378f42b68f5900adc23691214aacb8090eb732c5
Amine Abied <amine_abied@outlook.com>
2025-08-05 21:49:26 +0100

    Small adjust

--- checker.py
+++ checker.py
@@ -9,37 +9,37 @@ import sys
 app = Flask(__name__)
 
 status_data = {
-    "massar_up": False,
+    "website_up": False,
     "last_checked": None,
-    "message": "Massar site is currently down."
+    "message": "Website site is currently down."
 }
 
-MASSAR_URL = "https://cnc2025.ensem.ac.ma/"
+URL = "https://cnc2025.ensem.ac.ma/"
 
-def check_massar_status():
+def check_website_status():
     while True:
         try:
-            response = requests.get(MASSAR_URL, timeout=10)
+            response = requests.get(URL, timeout=10)
             timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
 
             if response.status_code == 200:
                 print(f"[{timestamp}] ✅ Massar is back online!")
-                status_data["massar_up"] = True
+                status_data["website_up"] = True
                 status_data["message"] = "✅ Massar is back online!"
                 status_data["last_checked"] = timestamp
 
-                webbrowser.open(MASSAR_URL)
+                webbrowser.open(URL)
                 print("✅ Browser opened. Exiting program now.")
                 sys.exit(0)
 
             else:
-                print(f"[{timestamp}] ❌ Massar still down (code {response.status_code})")
-                status_data["massar_up"] = False
+                print(f"[{timestamp}] ❌ Website still down (code {response.status_code})")
+                status_data["website_up"] = False
 
         except requests.RequestException as e:
             timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
             print(f"[{timestamp}] ❌ Error: {e}")
-            status_data["massar_up"] = False
+            status_data["website_up"] = False
 
         status_data["last_checked"] = time.strftime('%Y-%m-%d %H:%M:%S')
         time.sleep(random.uniform(1, 3))
@@ -52,11 +52,11 @@ def run_flask():
     app.run(host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
-    print("🟢 Starting Massar checker...")
+    print("🟢 Starting website checker...")
 
     # Start Flask in a thread
     flask_thread = threading.Thread(target=run_flask, daemon=True)
     flask_thread.start()
 
     # Run the checker in the main thread (so sys.exit() works)
-    check_massar_status()
+    check_website_status()
